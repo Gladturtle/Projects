08cba462563d168884ebe70227857654bd4389f0
Gladturtle <akashsam88@gmail.com>
2024-11-20 17:31:26 +0530

    Making different repositories for major projects

--- Image Storage API/test.py
+++
@@ -1,7 +0,0 @@
-import requests,os
-#list_of_dir = os.listdir('D:\Programs\Python\Projects\API\static')
-#for dir in list_of_dir:
-  #  print('D:\\Programs\\Python\\Projects\\API\\static\\'+dir)
-   # request = requests.put('http://127.0.0.1:5000/image/1',json={'img':'D:\\Programs\\Python\\Projects\\API\\static\\'+dir})
-request = requests.delete('http://127.0.0.1:5000/signup')
-print(request.json())
